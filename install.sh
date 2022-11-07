1253f5c80281916fe4b76e93c283212266ea3638
Lucas Harada <lucasyharada@gmail.com>
2022-11-07 16:32:25 -0300

    move devcontainer package installations to install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -euo pipefail
 
-# -e: exit on error
-# -u: exit on unset variables
-set -eu
+source "$(dirname $0)/install-chezmoi.sh"
 
-if ! chezmoi="$(command -v chezmoi)"; then
-  bin_dir="${HOME}/.local/bin"
-  chezmoi="${bin_dir}/chezmoi"
-  echo "Installing chezmoi to '${chezmoi}'" >&2
-  if command -v curl >/dev/null; then
-    chezmoi_install_script="$(curl -fsSL get.chezmoi.io)"
-  elif command -v wget >/dev/null; then
-    chezmoi_install_script="$(wget -qO- get.chezmoi.io)"
-  else
-    echo "To install chezmoi, you must have curl or wget installed." >&2
-    exit 1
-  fi
-  sh -c "${chezmoi_install_script}" -- -b "${bin_dir}"
-  unset chezmoi_install_script bin_dir
-fi
+# Install ripgrep
+sudo apt-get update && sudo apt-get install ripgrep
 
-# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
-script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-
-set -- init --apply --source="${script_dir}"
-
-echo "Running 'chezmoi $*'" >&2
-# exec: replace current process with chezmoi
-exec "$chezmoi" "$@"
+# Install delta
+wget -qO- https://github.com/dandavison/delta/releases/download/0.14.0/delta-0.14.0-aarch64-unknown-linux-gnu.tar.gz \
+| sudo tar xvz -C /usr/local/bin --wildcards '*/delta' --strip-components 1
